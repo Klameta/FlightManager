6832ae8456460044630d4564ff364d3eaf65b266
CatWizzard8790 <deniyankova1@gmail.com>
2024-03-29 16:37:46 +0200

    Created Users controller and index view

---
+++ FlightManager/ASPFlightManager/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPFlightManager.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly FlightManagerDbContext _context;
+
+        public UsersController(FlightManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users.ToListAsync());
+        }
+
+    }
+}
