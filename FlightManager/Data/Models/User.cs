ebaea60d48e7c8da0bff2a1acc5c682426be00ea
Klameta <ivanavalcheva@gmail.com>
2024-04-07 15:51:32 +0300

    validation done

--- FlightManager/Data/Models/User.cs
+++ FlightManager/Data/Models/User.cs
@@ -1,13 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Data.Models
 {
     public class User : IdentityUser
     {
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 50 characters.")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Last name must be between 2 and 50 characters.")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Social Security Number (SSN) is required.")]
         public string SSN { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        [StringLength(100, ErrorMessage = "Address cannot exceed 100 characters.")]
         public string Address { get; set; }
 
     }
