b785f379bc06ec2eb68221fc02483021a2700e62
CatWizzard8790 <deniyankova1@gmail.com>
2024-04-08 16:41:25 +0300

    cool titlesaa

---
+++ test/test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new SmtpClient("bulk.smtp.mailtrap.io", 587)
+            {
+                Credentials = new NetworkCredential("api", "e8761e829bb5029ab396344d43537310"),
+                EnableSsl = true
+            };
+            client.Send("mailtrap@srrairlines.duckdns.org", "ivanavalcheva@gmail.com", "Hello world", "testbody");
+        }
+    }
+}
